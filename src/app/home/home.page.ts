4e66159d0a2af2b5c3fd68117a98fa7320d8cd59
chanhuhu <luroman6@gmail.com>
2019-08-20 12:46:12 +0700

    3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import {log} from 'util';
+
 
 
 @Component({
@@ -10,20 +10,8 @@ import {log} from 'util';
 })
 export class HomePage {
   // tslint:disable-next-line:variable-name
-  users: any = [];
-  private url: any = '/assets/data.json';
-  constructor(public http: HttpClient) {
-    this.init();
-  }
 
-  init() {
-    this.getJson().subscribe((res) => {
-       console.log(res)
-      this.users = res;
-      // console.log(this.users)
-    });
-  }
-  getJson() {
-    return this.http.get(this.url);
-  }
+  constructor() { }
+
+
 }
