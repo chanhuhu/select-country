1d61e86f3dd95e666c9601adac9ba777d7d19beb
chanhuhu <luroman6@gmail.com>
2019-08-20 12:23:56 +0700

    1

---
+++ src/app/Models/dataModel.ts
@@ -0,0 +1,5 @@
+export class DataModel {
+    province: string;
+    amphoe: string;
+    district: string;
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {log} from 'util';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // tslint:disable-next-line:variable-name
+  users: any = [];
+  private url: any = '/assets/data.json';
+  constructor(public http: HttpClient) {
+    this.init();
+  }
 
-  constructor() {}
-
+  init() {
+    this.getJson().subscribe((res) => {
+      // console.log(res)
+      this.users = res;
+      // console.log(this.users)
+    });
+  }
+  getJson() {
+    return this.http.get(this.url);
+  }
 }
