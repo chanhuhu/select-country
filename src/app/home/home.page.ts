f9f2e89b3af558783dc5198b472dbd268608aea9
chanhuhu <luroman6@gmail.com>
2019-08-20 23:41:59 +0700

    mayBeEnd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,25 +9,23 @@ import { DataModel } from '../Models/dataModel';
 })
 export class HomePage {
   // tslint:disable-next-line:variable-name
-  filterX:any[]=[];
-  filterY:any[]=[];
-  filterZ:any[]=[];
+  filterX: any[] = [];
+  filterY: any[] = [];
+  filterZ: any[] = [];
   constructor(private userService: UserService) {
     this.init();
   }
   users;
-  sendData;
+  sendData1;
+  sendData2;
   init() {
     this.userService.getJson().subscribe((res) => {
-      // console.log(res);
       this.users = res;
       this.filterProvince();
-      // this.filterAmphoe();
-      // this.filterDistrict();
     });
   }
 
-  filterProvince(){
+  filterProvince() {
     for (let i = 0; i < this.users.length; i++) {
       let a = this.users[i].province;
       // tslint:disable-next-line:triple-equals
@@ -35,40 +33,31 @@ export class HomePage {
         this.filterX.push(a);
       }
     }
-    // console.log(this.filterX);
   }
-  searchProvince(name){
-    console.log(name);
-    let amphor:any = this.users.filter(data => data.province === name);
+  searchProvince(name) {
+    let amphoes: any = this.users.filter(data => data.province === name);
     this.filterY = [];
-    for (let i = 0; i < amphor.length; i++) {
-      let a = amphor[i].amphoe;
+    this.filterZ = [];
+    for (let i = 0; i < amphoes.length; i++) {
+      let a = amphoes[i].amphoe;
       // tslint:disable-next-line:triple-equals
       if (this.filterY.indexOf(a) == -1) {
         this.filterY.push(a);
       }
     }
+    console.log(this.filterY);
   }
-  // filterAmphoe(){
-  //   for (let i = 0; i < this.users.length; i++) {
-  //     let a = this.users[i].amphoe;
-  //     // tslint:disable-next-line:triple-equals
-  //     if (this.filterY.indexOf(a) == -1) {
-  //       this.filterY.push(a);
-  //     }
-  //   }
-  //
-  // }
-
-  filterDistrict(){
-    for (let i = 0; i < this.users.length; i++) {
-      let a = this.users[i].district;
+  searchAmphoe(Aname) {
+    let districts: any = this.users.filter(data => data.amphoe === Aname);
+    this.filterY = [];
+    this.filterZ = [];
+    for (let i = 0; i < districts.length; i++) {
+      let a = districts[i].district;
       // tslint:disable-next-line:triple-equals
       if (this.filterZ.indexOf(a) == -1) {
         this.filterZ.push(a);
       }
     }
-
   }
 
 }
