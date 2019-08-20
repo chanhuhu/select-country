03bc059512905a05031e3f941c1fb72e58938014
chanhuhu <luroman6@gmail.com>
2019-08-20 17:27:18 +0700

    finish

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,32 +11,50 @@ export class HomePage {
   // tslint:disable-next-line:variable-name
   filterX:any[]=[];
   filterY:any[]=[];
+  filterZ:any[]=[];
   constructor(private userService: UserService) {
     this.init();
   }
   users;
+  sendData;
   init() {
     this.userService.getJson().subscribe((res) => {
       // console.log(res);
       this.users = res;
       this.filterProvince();
+      this.filterAmphoe();
+      this.filterDistrict();
     });
   }
 
   filterProvince(){
-    for (let i=0;i<this.users.length;i++){
+    for (let i = 0; i < this.users.length; i++) {
       let a = this.users[i].province;
-      if (this.filterX.indexOf(a)==-1){
+      // tslint:disable-next-line:triple-equals
+      if (this.filterX.indexOf(a) == -1) {
         this.filterX.push(a);
       }
     }
-    console.log(this.filterX);
+    // console.log(this.filterX);
   }
-  // fliterAmphoe(){
-  //   for (let i=0;i<this.filterX.length,i++) {
-  //     if (this.filterX===this){
-  //
-  //     }
-  //   }
-  // }
+  filterAmphoe(){
+    for (let i = 0; i < this.users.length; i++) {
+      let a = this.users[i].amphoe;
+      // tslint:disable-next-line:triple-equals
+      if (this.filterY.indexOf(a) == -1) {
+        this.filterY.push(a);
+      }
+    }
+
+  }
+  filterDistrict(){
+    for (let i = 0; i < this.users.length; i++) {
+      let a = this.users[i].district;
+      // tslint:disable-next-line:triple-equals
+      if (this.filterZ.indexOf(a) == -1) {
+        this.filterZ.push(a);
+      }
+    }
+
+  }
 }
